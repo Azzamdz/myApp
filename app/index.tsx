e5aba8647c86d9b91ece2638841913f3910e959b
Azzamdz <azzamdzikri23.12.08@gmail.com>
2025-11-10 11:50:06 +0700

    feat router

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,166 @@
-// import LoginScreen from "@/components/tugas/loginScreen";
-// import Latihan1 from "@/components/latihan/latihan1";
-// import Latihan2 from "@/components/latihan/latihan2";
-// import Latihan3 from "@/components/latihan/latihan3";
-// import Latihan4 from "@/components/latihan/latihan4";
-// import HomeScreen from "@/components/tugas/HomeScreen";
-// import DetailScreen from "@/components/tugas/DetailScreen";
-import AddScreen from "@/components/tugas/AddScreen";
-// import UpdateScreen from "@/components/tugas/UpdateScreen";
+import { notes } from "@/data/noteData";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import {
+  FlatList,
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function Index() {
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type Note = {
+  id: number;
+  title: string;
+  desc: string;
+  date: string;
+  image: ImageSourcePropType | { uri: string };
+};
+
+const NoteItem = ({ item }: { item: Note }) => {
   return (
-    <>
-      {/* <Latihan1/> */}
-      {/* <Latihan2/> */}
-      {/* <Latihan3/> */}
-      {/* <Latihan4/> */}
-      {/* <LoginScreen/> */}
-      {/* <HomeScreen/> */}
-      {/* <DetailScreen /> */}
-      <AddScreen />
-      {/* <UpdateScreen /> */}
-    </>
+    <View style={styles.card}>
+      <Image style={{ width: 80, height: 80 }} source={item.image} />
+      <View style={styles.cardContainer}>
+        <Text style={styles.cardTitle}>{item.title}</Text>
+        <Text style={styles.cardDesc}>{item.desc}</Text>
+        <Text style={styles.cardDate}>{item.date}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default function HomeScreen() {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={{ width: 32, height: 32 }}
+          source={require("@/assets/images/logo.png")}
+        />
+        <Text style={styles.kodeinText}>Kodein</Text>
+        <Text style={styles.notesText}>Notes</Text>
+      </View>
+
+      <View style={styles.content}>
+        <FlatList
+          data={notes}
+          renderItem={({ item }) => <NoteItem item={item} />}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={{ gap: 16, flexGrow: 1 }}
+          ListEmptyComponent={() => <EmptyData />}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.fab} onPress={() => router.push("/add")}>
+        <Ionicons name="add" size={32} color="white" />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const EmptyData = () => {
+  return (
+    <View style={styles.emptyContainer}>
+      <Image
+        style={{ width: 150, height: 150 }}
+        source={require("@/assets/images/empty.png")}
+      />
+      <Text style={styles.emptyTitle}>Add your first note</Text>
+      <Text style={styles.emptyDesc}>
+        Save your thoughts, tasks or inspirations
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    gap: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e0e0e0",
+  },
+  kodeinText: {
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  notesText: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#FF5B13",
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "white",
+  },
+  card: {
+    elevation: 1,
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    backgroundColor: "#fff",
+  },
+  cardContainer: {
+    flex: 1,
+    padding: 8,
+    gap: 4,
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 4,
+    color: "#222",
+  },
+  cardDesc: {
+    fontSize: 14,
+    color: "#555",
+  },
+  cardDate: {
+    fontSize: 12,
+    color: "#999",
+  },
+  fab: {
+    position: "absolute",
+    bottom: 35,
+    right: 20,
+    backgroundColor: "#ff5813",
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+  },
+  emptyTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 8,
+  },
+  emptyDesc: {
+    fontSize: 16,
+    color: "gray",
+  },
+});
